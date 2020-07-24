b3e9818904a12d180d8d87b3e29c2a5235417788
1304452791 <57173197+1304452791@users.noreply.github.com>
2020-07-24 17:20:44 +0800

    账户流水以及修改账户金额

--- yixue-users/src/main/java/com/yixue/loxc/user/service/impl/UserWalletServiceImpl.java
+++ yixue-users/src/main/java/com/yixue/loxc/user/service/impl/UserWalletServiceImpl.java
@@ -2,15 +2,14 @@ package com.yixue.loxc.user.service.impl;
 
 import com.yixue.loxc.pojo.entity.TUserWalletEntity;
 import com.yixue.loxc.user.dao.UserWalletDao;
-import org.springframework.stereotype.Service;
-
 import com.yixue.loxc.user.service.UserWalletService;
+import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 
 
 @Service("userWalletService")
-public class UserWalletServiceImpl implements UserWalletService{
+public class UserWalletServiceImpl implements UserWalletService {
 
     @Resource
     UserWalletDao userWalletDao;
@@ -19,5 +18,13 @@ public class UserWalletServiceImpl implements UserWalletService{
     public TUserWalletEntity getwallet(String id) {
         TUserWalletEntity walletEntity = userWalletDao.selectById(id);
         return walletEntity;
+    }
+
+    @Override
+    public boolean update(TUserWalletEntity tUserWalletEntity) {
+        if (userWalletDao.update(tUserWalletEntity) > 0){
+            return true;
+        }
+        return false;
     }
 }
