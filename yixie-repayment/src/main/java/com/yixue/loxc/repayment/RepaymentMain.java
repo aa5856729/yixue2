5c0061bce5c71ac418ee1617f74419447f765d9b
1304452791 <57173197+1304452791@users.noreply.github.com>
2020-08-04 10:45:33 +0800

    没有运行之前的

--- yixie-repayment/src/main/java/com/yixue/loxc/repayment/RepaymentMain.java
+++ yixie-repayment/src/main/java/com/yixue/loxc/repayment/RepaymentMain.java
@@ -3,9 +3,11 @@ package com.yixue.loxc.repayment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.yixue.loxc.repayment", "com.yixue.loxc.user"})
+@ComponentScan(basePackages = {"com.yixue.loxc.repayment", "com.yixue.loxc.user", "com.yixue.loxc.loan"})
 public class RepaymentMain {
     public static void main(String[] args) {
         SpringApplication.run(RepaymentMain.class);
